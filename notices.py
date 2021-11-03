ef72f28ffed7cba5a44d1ffa1a3926afc664ee69
thisiscleverson <cleverson2020333@gmail.com>
2021-11-03 19:16:00 -0300

    notification

---
+++ notices.py
@@ -0,0 +1,45 @@
+import os
+import notify2
+from playsound import playsound
+from TypeFile import jsonList 
+from TypeFile import username
+
+
+def notifymeActivated():
+    Title = "File Organizer"
+    Message = "O programa foi iniciado!"
+
+    icon = username + jsonList['iconFoleder']
+    song = username + jsonList['Activated_program']
+
+    notify2.init("File Organizer")
+    n = notify2.Notification(Title,Message,icon)
+    n.show()
+    playsound(song)
+
+
+def notifymeErro():
+    Title = "File Organizer"
+    Message = "Ocorreu um erro na execulção do programa!"
+
+    icon = username + jsonList['Xicon']
+    song = username + jsonList['ErroSong']
+
+    notify2.init("File Organizer")
+    n = notify2.Notification(Title,Message,icon)
+    n.show()
+    playsound(song)
+
+
+def notifymeMoveFile(NameFile, NameFolder):
+    Title = "File Organizer"
+    Message = f'O arquivo "{NameFile}" foi movido para a pasta {NameFolder}'
+    icon = username + jsonList['FolderMove']
+    song = username + jsonList['movefileSong']
+
+    notify2.init("File Organizer")
+    n = notify2.Notification(Title,Message,icon)
+    n.show()
+    playsound(song)
+
+
