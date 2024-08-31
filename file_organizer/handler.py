526154e35fac3c299b092ff63d16aa425418cbdf
thisiscleverson <cleverson2020333@gmail.com>
2024-08-31 15:18:41 -0300

    adição da classe 'Notification' para mostrar notificações

--- file_organizer/handler.py
+++ file_organizer/handler.py
@@ -1,13 +1,14 @@
 import shutil
 from pathlib import Path 
 from watchdog.events import FileSystemEventHandler, FileSystemEvent
-from fileCategorize import FileCategorize
+
+from file_organizer.fileCategorize import FileCategorize
+from file_organizer.notification  import Notification
 
 
 class Handler(FileSystemEventHandler):
    def __init__(self, config_loader:dict) -> None:
       self.__file_categorize = FileCategorize(config_loader)
-
 
    def on_created(self, event:FileSystemEvent) -> None:
       self.move_file(event.src_path)
@@ -16,16 +17,18 @@ class Handler(FileSystemEventHandler):
    def move_file(self, path:str) -> None:
       destination_file = self.__file_categorize.get_destination_file(path)
 
-      if destination_file is None: return
-
-      """ if not Path(destination_file).exists():
-         Path(destination_file).mkdir(parents=True, exist_ok=True) """
+      if not destination_file:
+         return
 
       Path(destination_file).mkdir(parents=True, exist_ok=True)
 
+      filename = Path(path).name
+      
       try:
          shutil.move(path, destination_file)
-         print(f'file: {Path(path).stem} moved!')
-      
-      except:
-         raise Exception("It's not possible to move the file!")
+         
+      except Exception:
+         raise ValueError("It's not possible to move the file!")
+
+      notification = Notification("File Organizer", f'O arquivo "{filename}" foi movido para o diretorio: {destination_file}.')
+      notification.show()
