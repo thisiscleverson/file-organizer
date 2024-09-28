d152ae382dfbaf077f9b7bf1470e5cd4c3b5084a
thisiscleverson <cleverson2020333@gmail.com>
2024-09-27 23:21:52 -0300

    ajuste no tab space e adição do getattr para mostrar prints no ambiente de desenvolvimento

--- file_organizer/__main__.py
+++ file_organizer/__main__.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from watchdog.observers import Observer
 from pathlib import Path
 
@@ -8,25 +9,27 @@ from file_organizer.config_manager import ConfigManager
 
 
 def run(directory:str, handler:Handler) -> None:
-   observer = Observer()
+    observer = Observer()
 
-   observer.schedule(
-      handler, 
-      directory, 
-      recursive=False
-   )
-   observer.start()
+    observer.schedule(
+        handler, 
+        directory, 
+        recursive=False
+    )
+    observer.start()
 
-   #print(f'\n* Watcher Running in {directory}/\n')
+    # mostra o print se so estiver no ambiente de desenvolvimento!
+    if not getattr(sys, 'frozen', False):
+        print(f'\n* Watcher Running in {directory}/\n')
 
-   try:
-      while True:
-         time.sleep(1)
-   except Exception:
-      observer.stop()
-   except KeyboardInterrupt:
-      observer.stop()
-   observer.join()
+    try:
+        while True:
+            time.sleep(1)
+    except Exception:
+        observer.stop()
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 
 
@@ -34,11 +37,11 @@ config_manager = ConfigManager('config.json')
 handler        = Handler(config_manager)
 
 if __name__=="__main__":
-   user_home_directory = str(Path.home())
-   
-   directory = user_home_directory + config_manager.get_config("monitoringDirectory")
+    user_home_directory = str(Path.home())
+    
+    directory = user_home_directory + config_manager.get_config("monitoringDirectory")
 
-   run(
-      directory=directory if directory is not None else str(Path.home()) + '/Downloads',
-      handler=handler
-   )
+    run(
+        directory=directory if directory is not None else str(Path.home()) + '/Downloads',
+        handler=handler
+    )
