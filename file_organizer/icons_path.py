547bcf9492845c335edfb95273e79b1f515dafb8
thisiscleverson <cleverson2020333@gmail.com>
2024-09-27 23:26:47 -0300

    🔧 fix: corrigir caminhos dos ícones removendo './'

--- file_organizer/icons_path.py
+++ file_organizer/icons_path.py
@@ -2,6 +2,6 @@
 from enum import Enum
 
 class IconsPath(str, Enum):
-    ICON  = "./assets/icons/icon.png"
-    ERROR = "./assets/icons/error.png"
-    MOVED = "./assets/icons/moved.png"
+    ICON  = "assets/icons/icon.png"
+    ERROR = "assets/icons/error.png"
+    MOVED = "assets/icons/moved.png"
